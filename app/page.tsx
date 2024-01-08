0251d3fe56ce799e24a0573149adcbf2966e7a3c
Andre Ngo <andrengo94@gmail.com>
2024-01-08 01:07:14 -0800

    Setting up routing and adding button component from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <Button size="lg" variant="custom">
-      Button Text
-    </Button>
+    <h1>
+      Main Page
+    </h1>
   )
 }
